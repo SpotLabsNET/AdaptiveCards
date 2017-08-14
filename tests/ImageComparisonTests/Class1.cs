05f232c724c4728c9bc6484a023cc3633d923af9
Ricardo Arenas <riarenas@microsoft.com>
2017-08-14 10:27:14 -0700

    Create image comparison test project and basic image reading test

---
+++ tests/ImageComparisonTests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace ImageComparisonTests
+{
+    class ImageComparer
+    {
+        public static MagickImage ReadImage(string path)
+        {
+            try
+            {
+                var image = new MagickImage(path);
+                return image;
+            }
+            catch(MagickException e)
+            {
+                Console.WriteLine($"Exception while loading image file: {path}, Exception: {e}");
+                throw;
+            }
+        }
+
+    }
+}
